165adebd3031afb6a7f54ad33448451f02ea0813
Oluwanifemi <onayomide@gmail.com>
2023-07-15 17:12:22 +0000

    noobyte blogfolio commit

---
+++ site.config.ts
@@ -0,0 +1,66 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: '7d3aa449574744db86c642e0a07346d7',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Noobyte',
+  domain: 'nooby.te',
+  author: 'Oluwanifemi O.',
+
+  // open graph metadata (optional)
+  description: 'Oluwanifemi Blogfolio',
+
+  // social usernames (optional)
+  twitter: '0luwanifemi',
+  github: '0luwanifemi',
+  linkedin: 'oloyede-o',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: false,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+ // navigationStyle: 'default'
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: '83bd746ffdc44572b98bc78101ffa879'
+     },
+     {
+       title: 'Contact',
+       pageId: '1c340c70d7594efaa6f40fd81a3a350b'
+     }
+	
+   ]
+})
